edda1d577099823f2beb98dcdabf6f7eed2d8596
jamesbraid <james_braid@hotmail.co.uk>
2023-11-24 10:07:57 +0000

    id of {house} added to the card div on the character card page. Then styling added for each house name in sass so that each character's card is styled with the theme colours of their house. Text shadow added throughout the document.

--- src/components/CharCard.jsx
+++ src/components/CharCard.jsx
@@ -8,25 +8,25 @@ export default function CharCard(){
 
   return (
     <>
-    {/* <img src="src/styles/images/harry-potter-logo.png"  alt='house image' style={{width: '20rem'}}/> */}
-    {/* <img src={`src/styles/images/${house}.png`}/> */}
-      <div className='card-wrapper'>
-        <div className="card">
-          <img className="card-img-top" src={image} alt="Card image"/>
-          <div className="card-body">
-            <h5 className="card-title">{name}</h5>
+      <div className='crest-card'>
+        <img src={`../src/styles/images/${house}.jpeg`}/>
+        <div className='card-wrapper'>
+          <div className="card" id={house}>
+            <img className="card-img-top" src={image} alt="Card image"/>
+            <div className="card-body">
+              <h5 className="card-title">{name}</h5>
+            </div>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item">Wizard: {wizard ? 'Yes' : 'No'}</li>
+              <li className="list-group-item">House: {house ? house : 'N/A'}</li>
+              <li className="list-group-item">Patronus: {patronus ? patronus : 'N/A'}</li>
+              <li className="list-group-item">Wand Material: {wand.wood ? wand.wood : 'N/A' }</li>
+              <li className="list-group-item">Wand Core: {wand.core ? wand.core : 'N/A' }</li>
+              <li className="list-group-item">Wand Length: {wand.length ? `${wand.length} inches` : 'N/A' }</li>
+            </ul>
           </div>
-          <ul className="list-group list-group-flush">
-            <li className="list-group-item">Wizard: {wizard ? 'Yes' : 'No'}</li>
-            <li className="list-group-item">House: {house ? house : 'N/A'}</li>
-            <li className="list-group-item">Patronus: {patronus ? patronus : 'N/A'}</li>
-            <li className="list-group-item">Wand Material: {wand.wood ? wand.wood : 'N/A' }</li>
-            <li className="list-group-item">Wand Core: {wand.core ? wand.core : 'N/A' }</li>
-            <li className="list-group-item">Wand Length: {wand.length ? `${wand.length} inches` : 'N/A' }</li>
-          </ul>
+          <Link className='btn btn-red back' to="/charIndex">Back</Link>
         </div>
-        <Link className='btn btn-red back' to="/charIndex">Back</Link>
-        {/* <img src="src/styles/images/harry-potter-logo.png"  alt='house image' style={{width: '20rem'}}/> */}
       </div>
     </>
   )
